ecef8f88803ba45e23a5dd96908edd3723d05d59
lauralpt <laura.rodriguez@estudiantes.utec.edu.uy>
2020-09-15 23:35:22 -0300

    set

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,8 +10,7 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-'''
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-'''
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.dev")
+
 application = get_wsgi_application()
