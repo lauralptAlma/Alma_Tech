ea4740b4016bafe5fe9599c4324ee573292c53ce
MariaLauraGayo-utec <69489839+MariaLauraGayo-utec@users.noreply.github.com>
2020-09-18 21:47:58 -0300

    Agrego campo direccion (#4)

--- prueba/models.py
+++ prueba/models.py
@@ -28,7 +28,6 @@ SN_OPCIONES = (('NO', 'NO'), ('SI', 'SI'))
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_tipo = models.CharField(max_length=15, choices=USER_TIPO, default='')
-    user_direccion = models.CharField(max_length=155, null=True)
     user_celular = models.IntegerField(null=True)
     user_alta = models.DateField(blank=True, null=True)
     user_especialidad = models.CharField(max_length=15, choices=USER_ESPECIALIDAD, blank=True, null=True)
@@ -41,16 +40,17 @@ class UserProfile(models.Model):
 class Paciente(models.Model):
     # Informacion
     documento = models.CharField(primary_key=True, max_length=8, help_text="Sin puntos ni guión", null=False,
-                                 blank=True)
-    nombre = models.CharField(max_length=100)
-    primer_apellido = models.CharField(max_length=100, null=True, blank=False)
-    segundo_apellido = models.CharField(max_length=100, null=True, blank=False)
-    fecha_nacimiento = models.DateField('Fecha de nacimiento', help_text="ej. 01/08/2012", default=datetime.date.today)
+                                 blank=False)
+    nombre = models.CharField(max_length=100, null=False, blank=False)
+    primer_apellido = models.CharField(max_length=100, null=False, blank=False)
+    segundo_apellido = models.CharField(max_length=100, null=True, blank=True)
+    direccion = models.CharField(max_length=155, null=False, blank=False)
+    fecha_nacimiento = models.DateField('Fecha de nacimiento', help_text="ej. 01/08/2012", default=datetime.date.today, null=False, blank=False)
     # Contacto
     celular_regex = RegexValidator(regex=r'^\+?1?\d{9,9}$',
                                    message="El número debe ser del formato: '+XXXXXXXXX'. 9 digitos admitidos.")
     celular = models.CharField("Número de teléfono celular", validators=[celular_regex], max_length=9, unique=True,
-                               null=False, blank=True)  # validators should be a list
+                               null=False, blank=False)  # validators should be a list
     email = models.EmailField(max_length=200, unique=True, blank=True)
     # Nucleo
     alta = models.DateTimeField(auto_now_add=True),
