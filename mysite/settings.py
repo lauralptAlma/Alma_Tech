deaf690f4d891e2acfd678d568a04054c0a97b5f
Maria Laura Gayo <maria.gayo@estudiantes.utec.edu.uy>
2020-10-03 16:17:21 -0300

    Cambio base de datos

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
-        "CLIENT": {"name": 'pruebacpomdb',
+        "CLIENT": {"name": 'dentaledb',
                    "host": 'mongodb+srv://admin:almaAdmin@clusteralmatech.izim4.mongodb.net/test?authSource=admin&readPreference=primary&ssl=true',
                    "username": 'admin',
                    "password": 'almaAdmin',
