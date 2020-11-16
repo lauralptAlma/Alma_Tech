0d1a77ae6565c379c8d15b1f574074be727901ad
Maria Laura Gayo <maria.gayo@estudiantes.utec.edu.uy>
2020-11-16 18:25:01 -0300

    Installed apps

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
-INSTALLED_APPS = (
+INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,7 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'bootstrap_datepicker_plus',
     'rest_framework',
-)
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
